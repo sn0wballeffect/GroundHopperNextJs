5b565ad6f16fa37ddda8b6cd6a1cf7a4698c0232
Sn0wballEffect <Matthaeus2508@icloud.com>
2024-11-23 00:33:56 +0100

    s

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -2,8 +2,16 @@ import { SearchComponent } from "../../components/SearchComponent";
 
 export default function SearchPage() {
   return (
-    <div className="flex justify-center h-full w-full ">
+    <div className="flex flex-col justify-center h-full w-full ">
       <SearchComponent />
+      <div className="grid grid-cols-1 p-1 md:grid-cols-5 h-[calc(100vh-64px)] overflow-hidden">
+        <div className="col-span-2 overflow-auto">
+          HAAAAAAAAAAAAAAAAAAALLLLLLLLLLLLLLLLLLLOOOOOOOOOOO
+        </div>
+        <div className="col-span-3 hidden md:block h-full mt-1 pb-1">
+          HAAAAAAAAAAAAAAAAAAAAAAAAAAALLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOO
+        </div>
+      </div>
     </div>
   );
 }

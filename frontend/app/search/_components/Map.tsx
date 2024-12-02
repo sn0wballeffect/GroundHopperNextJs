efd035c711888eabad8e776eba31588a34082a8f
Sn0wballEffect <Matthaeus2508@icloud.com>
2024-12-02 23:09:10 +0100

    gg

--- frontend/app/search/_components/Map.tsx
+++ frontend/app/search/_components/Map.tsx
@@ -5,7 +5,6 @@ we need to make this component client rendered as well*/
 import { useStore } from "@/lib/store";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 import { useRef, useEffect, useState } from "react";
-import stadiumData from "@/data/stadium_data.json";
 
 //Map's styling
 const defaultMapContainerStyle = {
@@ -102,19 +101,6 @@ const MapComponent = () => {
         onLoad={onLoad}
       >
         {markerPosition && <Marker position={markerPosition} />}
-        {stadiumData.slice(0, 50).map((stadium, index) => {
-          const stadiumName = Object.keys(stadium)[0];
-          const coordinates = stadium[stadiumName].Coordinates;
-          const link = stadium.Link;
-          return (
-            <Marker
-              key={index}
-              position={{ lat: coordinates[0], lng: coordinates[1] }}
-              title={stadiumName}
-              onClick={() => window.open(link, "_blank")}
-            />
-          );
-        })}
       </GoogleMap>
     </div>
   );

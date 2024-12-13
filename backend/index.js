5ecd1ae5ceccff2436726f41ed6aa269c1af2763
Sn0wballEffect <Matthaeus2508@icloud.com>
2024-12-13 11:26:36 +0100

    vps

--- backend/index.js
+++ backend/index.js
@@ -168,6 +168,6 @@ app.get("/cities", async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });

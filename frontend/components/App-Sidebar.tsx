33d8729190176dab1fa7cc75dceb9b03d0ca778e
Sn0wballEffect <Matthaeus2508@icloud.com>
2024-12-19 07:07:53 +0100

    Sidebar still

--- frontend/components/App-Sidebar.tsx
+++ frontend/components/App-Sidebar.tsx
@@ -5,16 +5,25 @@ import {
   SidebarGroup,
   SidebarHeader,
   SidebarTrigger,
+  SidebarGroupLabel,
+  SidebarGroupContent,
 } from "@/components/ui/sidebar";
+import { NavUser } from "./sidebarUser";
 
 export function AppSidebar() {
+  // You can either define the user object here
+  const user = {
+    name: "John Doe",
+    email: "john@example.com",
+    avatar: "/path/to/avatar.jpg",
+  };
+
   return (
     <Sidebar side="right">
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
+      <SidebarHeader className="h-16 border-b border-sidebar-border">
+        <NavUser user={user} />
+      </SidebarHeader>
+      <SidebarContent>Sortierung</SidebarContent>
       <SidebarFooter />
     </Sidebar>
   );
